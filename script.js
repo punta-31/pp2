a53c2ee5eb814b2b7833ddb3017b7ab7aa8a8f58
punta-31 <puntaangsilp@gmail.com>
2025-09-09 15:56:09 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+contact.html
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger-menu');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    if (hamburger && mobileMenu) {
+        hamburger.addEventListener('click', () => {
+            mobileMenu.classList.toggle('active');
+            hamburger.querySelector('i').classList.toggle('fa-bars');
+            hamburger.querySelector('i').classList.toggle('fa-times');
+        });
+    }
+});
